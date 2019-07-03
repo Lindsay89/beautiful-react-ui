aca9d814db2597adc6940be9eaad62cd1ee57688
Francesca Maria Lindsay Palermo <francesca@FrancescasMBP2.station>
2019-07-03 17:51:06 +0200

    started modal component

---
+++ src/components/Elements/Modal/ModalFooter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const ModalFooter = (props) => {
+  const { children } = props;
+  const classList = classNames('bi-modal-footer');
+  return (
+    <footer className={classList}>{children}</footer>
+  );
+};
+
+ModalFooter.propTypes = {
+  children: PropTypes.node,
+};
+
+ModalFooter.defaultProps = {
+  children: undefined,
+};
+
+export default React.memo(ModalFooter);
