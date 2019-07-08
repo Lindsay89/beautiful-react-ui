fc9a5e76c1482ca42789765674e2cf7e3d9b7b95
Francesca Maria Lindsay Palermo <francesca@FrancescasMBP2.station>
2019-07-08 14:59:51 +0200

    some minor fix

--- src/components/Elements/Modal/Modal.js
+++ src/components/Elements/Modal/Modal.js
@@ -45,6 +45,10 @@ const Modal = (props) => {
     'modal-slideTop': animation === 'slideTop',
   }, className);
 
+  const classBackdrop = classNames({
+    'modal-backdrop': !backdropRender,
+    'backdrop-render': backdropRender,
+  });
   let modalDiv = document.getElementById('bi-modals');
 
   useEffect(() => {
@@ -71,24 +75,22 @@ const Modal = (props) => {
   };
 
   if (isOpen) {
-    if (backdropRender) {
-      backdropRender(props);
-    } else {
-      if (onShow) {
-        onShow();
-      }
-      // one of the two following props must be defined.
-      if (!onToggle && !closeButtonRender) {
-        warn('It must be define one of the following two props to close the modal: onToggle or closeButtonRender');
-      }
+    if (onShow) {
+      onShow();
+    }
+    // one of the two following props must be defined.
+    if (!onToggle && !closeButtonRender) {
+      warn('It must be define one of the following two props to close the modal: onToggle or closeButtonRender');
+    }
 
-      /**
-       * createPortal has been used to moved the component into a div that will be always in front of the main window
-       * if open.
-       * This div won't be in the "original" DOM tree but it will be the latest div into the entire DOM.
-       */
-      return ReactDOM.createPortal(
-        <div className="bi-show-modal" onClick={onBackdropClick} role="presentation">
+    /**
+     * createPortal has been used to moved the component into a div that will be always in front of the main window
+     * if open.
+     * This div won't be in the "original" DOM tree but it will be the latest div into the entire DOM.
+     */
+    return ReactDOM.createPortal(
+      <div className="bi-show-modal" onClick={onBackdropClick || backdropRender} role="presentation">
+        <div className={classBackdrop}>
           <div
             id={id}
             style={style}
@@ -98,18 +100,18 @@ const Modal = (props) => {
           >
             {closeButtonRender && closeButtonRender(props)}
             {!closeButtonRender
-            && (
-              <Button color="transparent" className="close-button" onClick={onCloseFunctions}>
-                <CloseIcon />
-              </Button>
-            )
+              && (
+                <Button color="transparent" className="close-button" onClick={onCloseFunctions}>
+                  <CloseIcon />
+                </Button>
+              )
             }
             {Children.map(children, child => wipeOutIncorrectChildren(child))}
           </div>
-        </div>,
-        modalDiv,
-      );
-    }
+        </div>
+      </div>,
+      modalDiv,
+    );
   }
 
   return null;
--- src/components/Elements/Modal/README.md
+++ src/components/Elements/Modal/README.md
@@ -87,7 +87,7 @@ const ModalController = (props) => {
 
 ```
 
-### Size
+### Sizes
 `size` props could be used to decide the modal size.
 
 ```jsx
@@ -100,7 +100,7 @@ const ModalController = (props) => {
   <Modal {...props} isOpen={value} onToggle={()=>{setValue(!value)}}>
     <Modal.Title>Amazing modal title</Modal.Title>
     <Modal.Body>
-      Zombie ipsum brains reversus ab cerebellum viral inferno, brein nam rick mend grimes malum cerveau cerebro. De carne cerebro lumbering animata cervello corpora quaeritis. Summus thalamus brains sit​​, morbo basal ganglia vel maleficia? De braaaiiiins apocalypsi gorger omero prefrontal cortex undead survivor fornix dictum mauris. 
+      Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead. Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague haec decaying ambulabat mortuos. Sicut zeder apathetic malus voodoo. Aenean a dolor plan et terror soulless vulnerum contagium accedunt, mortui iam vivam unlife. Qui tardius moveri, brid eof reanimator sed in magna copia sint terribiles undeath legionis. Alii missing oculis aliorum sicut serpere crabs nostram. Putridi braindead odores kill and infect, aere implent left four dead.
     </Modal.Body>
     <Modal.Footer>
       <Button>Adios!</Button>
@@ -252,7 +252,7 @@ const ModalController = (props) => {
 ```jsx
 import {Button} from 'beautiful-ui';
 
-const backDrop = () => alert('you define just an alert, do you expect something more??');
+const backDrop = (props) => (<div>{props.children}</div>);
 
 const ModalController = (props) => {
   const [value, setValue] = React.useState(false);
