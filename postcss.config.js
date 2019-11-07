9fcc061f53af6086840d1b63bdf2430f44271eb6
Antonio Russo <antonio@beautifulinteractions.com>
2019-11-07 15:36:48 +0000

    Optimizing CSS build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,17 @@
-const PostCSSPresetEnv = require('postcss-preset-env');
-const Tailwind = require('tailwindcss');
-const Autoprefixer = require('autoprefixer');
-
+/* eslint-disable import/no-extraneous-dependencies, global-require */
 module.exports = {
   plugins: [
-    PostCSSPresetEnv({ stage: 0 }),
-    Tailwind('./tailwind.config.js'),
-    Autoprefixer,
+    require('postcss-normalize')({ browsers: 'last 2 versions' }),
+    require('postcss-preset-env')({ stage: 0 }),
+    require('tailwindcss')('./tailwind.config.js'),
+    require('postcss-fixes'),
+    require('autoprefixer'),
+    require('postcss-will-change-transition'),
+    require('postcss-will-change'),
+    require('cssnano')({
+      preset: 'default',
+      safe: true, // I would recommend using cssnano only in safe mode
+      calc: false, // calc is no longer necessary, as it is already done by postcss-fixes
+    }),
   ],
 };
